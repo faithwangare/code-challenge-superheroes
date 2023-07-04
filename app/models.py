5d3b3bd380a7450acf89ff45fcc09d2d0c498d7e
faithwangare <faithwangare750@gmail.com>
2023-07-04 21:25:07 +0300

    created models.py

--- app/models.py
+++ app/models.py
@@ -6,5 +6,23 @@ class Hero(db.Model):
     __tablename__ = 'hero'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    super_name = db.Column(db.String(255), nullable=False)
+    powers = db.relationship('Power', secondary='hero_power', backref='heroes')
 
-# add any models you may need. 
+class Power(db.Model):
+    __tablename__ = 'power'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+
+class HeroPower(db.Model):
+    __tablename__ = 'hero_power'
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String(255), nullable=False)
+    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
+    hero = db.relationship('Hero', backref=db.backref('hero_powers', cascade='all, delete-orphan'))
+    power = db.relationship('Power', backref=db.backref('hero_powers', cascade='all, delete-orphan'))
